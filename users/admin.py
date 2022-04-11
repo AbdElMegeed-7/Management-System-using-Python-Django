571a7cd94577e5eec3295fd73cb56413ae0da24d
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-04-11 23:52:40 +0200

    admin register models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Profile, Grade, Section, Question, Answer
 # Register your models here.
+
+
+admin.site.register(Profile)
+admin.site.register(Grade)
+admin.site.register(Section)
+admin.site.register(Question)
+admin.site.register(Answer)
