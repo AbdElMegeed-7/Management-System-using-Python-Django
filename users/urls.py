9594e16d5e16927671a2496a7f5054b26794c47d
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-04-13 23:21:51 +0200

    Profile

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    # path('homework2/<int:pk>', views.HomeworkDetailView.as_view(), name='homework2'),
+    # path('section_list/', views.SectionListView.as_view(), name='section_list'),
+    # path('section_detail/<int:pk>',
+    #      views.SectionDetailView.as_view(), name='section_detail'),
+    # path('profile_detail/<int:pk>',
+    #      views.ProfileDetailView.as_view(), name='profile_detail'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,29 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
 def home(request):
     return render(request, 'users/home.html')
+
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Profile successfully Updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {'u_form': u_form, 'p_form': p_form}
+    return render(request, 'users/profile.html', context)
 
 
 def register(request):
@@ -23,8 +42,5 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 
-def profile(request):
-    return 
-
 def results(request):
-    return 
+    return
