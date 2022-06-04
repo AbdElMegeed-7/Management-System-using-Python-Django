3a876e008bd98eca44c8fba14d34528d083a6b73
AbdElMegeed <AbdElMegeedX7@gmail.com>
2022-06-04 11:03:09 +0400

    DATABASE dj_database_url

--- ManagementSystem/settings.py
+++ ManagementSystem/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,11 +78,15 @@ WSGI_APPLICATION = 'ManagementSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'mydatabase',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'mydatabase',
-    }
+    'default': dj_database_url.config(default='sqlite://db/mydatabase')
 }
 
 
