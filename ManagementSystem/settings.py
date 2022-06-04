8075704742d96142905dbf222dccd1fdd0ebc8e2
AbdElMegeed <AbdElMegeedX7@gmail.com>
2022-06-04 11:49:00 +0400

    django-heroku

--- ManagementSystem/settings.py
+++ ManagementSystem/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -152,3 +154,6 @@ LOGIN_URL = 'login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+django_heroku.settings(locals())
